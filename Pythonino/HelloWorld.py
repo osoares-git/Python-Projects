4135ded17567464e3c9ad1c004281242d2a64902
orlindo <orlindowagner@gmail.com>
2020-03-26 11:46:09 -0300

    primeira mudança feita somente no script .py

--- Pythonino/HelloWorld.py
+++ Pythonino/HelloWorld.py
@@ -1,14 +1,14 @@
 from pyfirmata import Arduino, util
 import time
 
-Mega = Arduino('COM6')
+Mega = Arduino('COM4')
 
 print('Hello World!')
 
 while True:
     Mega.digital[13].write(1)
     print('LED ligado')
-    time.sleep(0.9)
+    time.sleep(0.1)
 
     Mega.digital[13].write(0)
     print('LED desligado')
